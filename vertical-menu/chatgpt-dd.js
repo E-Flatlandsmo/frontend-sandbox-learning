6188e4adf855219988355d46d2cc0769a576df01
eivind-flatlandsmo-4ss <89027820+eivind-flatlandsmo-4ss@users.noreply.github.com>
2023-06-23 10:10:55 +0200

    deleted files

--- vertical-menu/chatgpt-dd.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const verticalMenu = document.querySelector('.vertical-menu');
-
-menuToggle.addEventListener('click', function() {
-    verticalMenu.classList.toggle('open');
-    console.log(verticalMenu)
-});
-// 
