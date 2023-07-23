cfd2eae2d65ad5c6d2f223fb83eb1e1ebe708873
eivind-flatlandsmo-4ss <89027820+eivind-flatlandsmo-4ss@users.noreply.github.com>
2023-07-23 14:05:29 +0200

    menu icon added

---
+++ mobile-menu-with-frontpage/scripts/menu.js
@@ -0,0 +1,9 @@
+function show() {
+    const verticalMenu = document.querySelector('.vertical-navbar');
+    verticalMenu.classList.add('menu-toggle')
+}
+
+function hide() {
+    const verticalMenu = document.querySelector('.vertical-navbar');
+    verticalMenu.classList.remove('menu-toggle')
+}
