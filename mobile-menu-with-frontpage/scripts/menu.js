cf6af4c8447eb530329450529c4aae0cc42f437f
eivind-flatlandsmo-4ss <89027820+eivind-flatlandsmo-4ss@users.noreply.github.com>
2023-07-24 12:26:46 +0200

    working version mobile menu and landing page

--- mobile-menu-with-frontpage/scripts/menu.js
+++ mobile-menu-with-frontpage/scripts/menu.js
@@ -1,9 +1,9 @@
 function show() {
-    const verticalMenu = document.querySelector('.vertical-navbar');
+    const verticalMenu = document.querySelector('.mobile-navbar');
     verticalMenu.classList.add('menu-toggle')
 }
 
 function hide() {
-    const verticalMenu = document.querySelector('.vertical-navbar');
+    const verticalMenu = document.querySelector('.mobile-navbar');
     verticalMenu.classList.remove('menu-toggle')
 }
